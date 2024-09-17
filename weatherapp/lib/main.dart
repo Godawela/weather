7a6fd9154fed4244349fc584c9e0391413e969bd
Godawela <godawelaus.21@uom.lk>
2024-09-18 02:58:19 +0530

    issue in the gradle not fixed yet

--- weatherapp/lib/main.dart
+++ weatherapp/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'ui/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "My App",
-      home: HomePage(),
+      
       debugShowCheckedModeBanner: false,
     );
 }
